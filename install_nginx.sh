e6f64aba042cde782b2530eb7bfeb01c6b388fe3
alfaiz-deshmukh <alfaizdeshmukh2@gmail.com>
2026-02-19 10:07:20 +0000

    Boom in the chat

--- install_nginx.sh
+++ install_nginx.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# This scripts is for install nginx
+# This scripts is for install ngin
+
 sudo apt-get update 
 
 sudo apt install nginx 
@@ -10,3 +11,7 @@ sudo systemctl start nginx
 sudo systemctl enable nginx 
 
 echo "Successfully NGINX INSTALL"
+
+#boom in the chat 
+#just for practice 
+
